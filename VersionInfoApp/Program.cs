b77bff0b4388a698c115e2d5317fd2d19ff1dafb
kzmiyamura <kzmiyamura@gmail.com>
2026-01-08 19:32:51 +0900

    Initial commit: VersionInfoApp console app

---
+++ VersionInfoApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace VersionInfoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var version = assembly.GetName().Version?.ToString() ?? "unknown";
+            var infoVersion = assembly
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion ?? "unknown";
+
+            Console.WriteLine("=== Version Info ===");
+            Console.WriteLine($"Assembly Version           : {version}");
+            Console.WriteLine($"Assembly InformationalVersion (Git Hash) : {infoVersion}");
+        }
+    }
+}
